47019110688ce9c0bdc0eaab7e5e26efa1145e9f
Jatin Karthik Tripathy <jatinkarthikt@gmail.com>
2020-06-30 01:02:31 +0530

    added persistant log in

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:app/screens/auth.dart';
+import 'package:app/screens/home.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:animated_splash/animated_splash.dart';
+
 void main() {
   runApp(
     MaterialApp(
@@ -27,7 +30,66 @@ class MyApp extends StatelessWidget {
         primaryColor: Color(0xFFF7F8F3),
         accentColor: Color(0xFFFF6F5E),
       ),
-      home: Auth(),
+      home: Root(),
     );
+  }
+}
+
+enum AuthStatus { notLoggedIn, loggedIn }
+
+class Root extends StatefulWidget {
+  @override
+  _RootState createState() => _RootState();
+}
+
+class _RootState extends State<Root> {
+  AuthStatus _authStatus = AuthStatus.notLoggedIn;
+  String uid;
+  String name;
+  String imageUrl;
+
+  FirebaseAuth _auth = FirebaseAuth.instance;
+
+  Future<String> onStart() async {
+    String ret = "error";
+    try {
+      FirebaseUser _user = await _auth.currentUser();
+      uid = _user.uid;
+      name = _user.displayName;
+      imageUrl = _user.photoUrl;
+      ret = "Success";
+    } catch (e) {}
+    return ret;
+  }
+
+  @override
+  void didChangeDependencies() async {
+    super.didChangeDependencies();
+    String retString = await onStart();
+    if (retString == "Success") {
+      setState(() {
+        _authStatus = AuthStatus.loggedIn;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Widget retVal;
+    switch (_authStatus) {
+      case AuthStatus.notLoggedIn:
+        retVal = Auth();
+        break;
+      case AuthStatus.loggedIn:
+        retVal = HomePage(
+          title: "Drabble",
+          uid: uid,
+          name: name,
+          imageUrl: imageUrl,
+        );
+        break;
+      default:
+    }
+    return retVal;
   }
 }
